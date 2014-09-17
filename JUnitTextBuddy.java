93fe471ff850a592d175d306a7fbe5d335d35315
Antony <karukapallyantony@msn.com>
2014-09-17 20:32:54 +0800

    set up the class

--- JUnitTextBuddy.java
+++ JUnitTextBuddy.java
@@ -1,13 +1,18 @@
 import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-
-public class JUnitTextBuddy {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+	
+	import java.io.BufferedReader;
+	import java.io.File;
+	import java.io.FileReader;
+	import java.io.IOException;
+	import java.util.ArrayList;
+	
+	import org.junit.BeforeClass;
+	import org.junit.Test;
+	
+	
+	public class JUnitTextBuddy {
+	
+		@BeforeClass
+		public static void setUpBeforeClass() throws Exception {
+		}
+	
