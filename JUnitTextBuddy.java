b64f955a2fed637335771a7fbda7f8042e4f0b01
Antony <karukapallyantony@msn.com>
2014-09-18 14:56:44 +0800

    sort test

--- JUnitTextBuddy.java
+++ JUnitTextBuddy.java
@@ -9,10 +9,36 @@ import static org.junit.Assert.*;
 	import org.junit.BeforeClass;
 	import org.junit.Test;
 	
-	
+	//Antony K
 	public class JUnitTextBuddy {
 	
 		@BeforeClass
 		public static void setUpBeforeClass() throws Exception {
 		}
-	
+		@Test
+		public void testSort(){
+
+			ArrayList<String> expectedArray = new ArrayList<String>();
+			
+			String firstLine = "A first";
+			String lastLine = "Z Last";
+			String middleLine = "M middle";
+			
+			expectedArray.add(firstLine);
+			expectedArray.add(middleLine);
+			expectedArray.add(lastLine);
+			
+			TextBuddy testSoftware = new TextBuddy("test.txt");
+			
+			testSoftware.addContent(lastLine);
+			testSoftware.addContent(firstLine);
+			testSoftware.addContent(middleLine);
+			
+			assertFalse(expectedArray.equals(testSoftware.content));
+			
+			testSoftware.sortContent();
+			
+			assertEquals(expectedArray,testSoftware.content);
+			
+			testSoftware.clearContent();
+		}
